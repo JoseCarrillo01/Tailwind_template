cc7c15cea59b972d1ba1171d84374775ce283f1a
JoseCarrillo01 <josecarrival1@gmail.com>
2024-02-02 12:19:34 -0700

    ReactAndTailwindv2.1

--- src/components/bloc.js
+++ src/components/bloc.js
@@ -169,6 +169,8 @@ const posts = [
   
   export default function Bloc() {
     return (
+
+        <div>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3" />
@@ -223,6 +225,117 @@ const posts = [
             ))}
           </div>
         </div>
+      </div>
+
+
+      <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+        <div>
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Press</h2>
+          <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
+            <p className="text-xl text-gray-500">Get weekly articles in your inbox on how to grow your business.</p>
+            <form className="mt-6 flex flex-col sm:flex-row lg:mt-0 lg:justify-end">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email-address"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none w-full px-4 py-2 border border-gray-300 text-base rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 lg:max-w-xs"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div className="mt-2 flex-shrink-0 w-full flex rounded-md shadow-sm sm:mt-0 sm:ml-3 sm:w-auto sm:inline-flex">
+                <button
+                  type="button"
+                  className="w-full bg-indigo-600 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:inline-flex"
+                >
+                  Notify me
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
+          {posts.map((post) => (
+            <div key={post.title}>
+              <p className="text-sm text-gray-500">
+                <time dateTime={post.datetime}>{post.date}</time>
+              </p>
+              <a href="#" className="mt-2 block">
+                <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                <p className="mt-3 text-base text-gray-500">{post.description}</p>
+              </a>
+              <div className="mt-3">
+                <a href={post.href} className="text-base font-semibold text-indigo-600 hover:text-indigo-500">
+                  Read full story
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+      
+
+    <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+        <div>
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Recent publications</h2>
+          <p className="mt-3 text-xl text-gray-500 sm:mt-4">
+            Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus
+            arcu.
+          </p>
+        </div>
+        <div className="mt-12 grid gap-16 pt-12 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
+          {posts.map((post) => (
+            <div key={post.title}>
+              <div>
+                <a href={post.category.href} className="inline-block">
+                  <span
+                    className={classNames(
+                      post.category.color,
+                      'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium'
+                    )}
+                  >
+                    {post.category.name}
+                  </span>
+                </a>
+              </div>
+              <a href={post.href} className="block mt-4">
+                <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                <p className="mt-3 text-base text-gray-500">{post.description}</p>
+              </a>
+              <div className="mt-6 flex items-center">
+                <div className="flex-shrink-0">
+                  <a href={post.author.href}>
+                    <span className="sr-only">{post.author.name}</span>
+                    <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt="" />
+                  </a>
+                </div>
+                <div className="ml-3">
+                  <p className="text-sm font-medium text-gray-900">
+                    <a href={post.author.href}>{post.author.name}</a>
+                  </p>
+                  <div className="flex space-x-1 text-sm text-gray-500">
+                    <time dateTime={post.datetime}>{post.date}</time>
+                    <span aria-hidden="true">&middot;</span>
+                    <span>{post.readingTime} read</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+      
+      
+      
       </div>
     )
   }
